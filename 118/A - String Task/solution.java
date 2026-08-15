a1bb87f1945cec09a5acb69b81acbfeb923f1cef
Abhmanyu Singh <abhimanyu15282005@gmail.com>
2026-08-15 09:33:40 +0530

    Add String Task [A] from Codeforces

---
+++ 118/A - String Task/solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+ 
+        String s = sc.next().toLowerCase();
+ 
+        StringBuilder ans = new StringBuilder();
+ 
+        for (char c : s.toCharArray()) {
+ 
+            if (c != 'a' && c != 'e' && c != 'i' &&
+                c != 'o' && c != 'u' && c != 'y') {
+ 
+                ans.append('.');
+                ans.append(c);
+            }
+        }
+ 
+        System.out.println(ans);
+ 
+        sc.close();
+    }
+}

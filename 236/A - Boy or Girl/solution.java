a8559a79618d16ce6f0c9187c3e7ab1cceca7a7c
Abhmanyu Singh <abhimanyu15282005@gmail.com>
2026-08-15 09:32:59 +0530

    Add Boy or Girl [A] from Codeforces

---
+++ 236/A - Boy or Girl/solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+ 
+        String s = sc.next();
+ 
+        boolean[] seen = new boolean[26];
+        int count = 0;
+ 
+        for (char c : s.toCharArray()) {
+            if (!seen[c - 'a']) {
+                seen[c - 'a'] = true;
+                count++;
+            }
+        }
+ 
+        if (count % 2 == 0)
+            System.out.println("CHAT WITH HER!");
+        else
+            System.out.println("IGNORE HIM!");
+ 
+        sc.close();
+    }
+}

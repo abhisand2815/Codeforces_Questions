5f8f273f986dec1d33fb0c822adadb8f45798629
Abhmanyu Singh <abhimanyu15282005@gmail.com>
2026-08-15 09:35:04 +0530

    Add Twins [A] from Codeforces

---
+++ 160/A - Twins/solution.java
@@ -0,0 +1,34 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+ 
+        int n = sc.nextInt();
+        int[] coins = new int[n];
+ 
+        int total = 0;
+ 
+        for (int i = 0; i < n; i++) {
+            coins[i] = sc.nextInt();
+            total += coins[i];
+        }
+ 
+        Arrays.sort(coins);
+ 
+        int mine = 0;
+        int count = 0;
+ 
+        for (int i = n - 1; i >= 0; i--) {
+            mine += coins[i];
+            count++;
+ 
+            if (mine > total - mine) {
+                break;
+            }
+        }
+ 
+        System.out.println(count);
+    }
+}

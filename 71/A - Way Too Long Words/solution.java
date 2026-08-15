50b1365696158b5383ad20aa7243567728a15d89
Abhmanyu Singh <abhimanyu15282005@gmail.com>
2026-08-15 09:32:25 +0530

    Add Way Too Long Words [A] from Codeforces

---
+++ 71/A - Way Too Long Words/solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int n = sc.nextInt();
+ 
+        while (n-- > 0) {
+            String s = sc.next();
+ 
+            if (s.length() <= 10) {
+                System.out.println(s);
+            } else {
+                System.out.println(
+                        s.charAt(0)
+                        + String.valueOf(s.length() - 2)
+                        + s.charAt(s.length() - 1)
+                );
+            }
+        }
+ 
+        sc.close();
+    }
+}

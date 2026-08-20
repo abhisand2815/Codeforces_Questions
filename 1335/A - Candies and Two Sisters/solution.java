6cfc9879a4ae1471e23c6718e316797730757e97
Abhmanyu Singh <abhimanyu15282005@gmail.com>
2026-08-20 09:05:09 +0530

    Add Candies and Two Sisters [A] from Codeforces

---
+++ 1335/A - Candies and Two Sisters/solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            long n = sc.nextLong();
+ 
+            if (n < 3) {
+                System.out.println(0);
+            } else {
+                System.out.println((n - 1) / 2);
+            }
+        }
+ 
+        sc.close();
+    }
+}

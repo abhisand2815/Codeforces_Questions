dd9d1ca2e99e6e5e059bc88b4dd58bbe9b4dbce8
Abhmanyu Singh <abhimanyu15282005@gmail.com>
2026-08-22 08:21:53 +0530

    Add IQ test [A] from Codeforces

---
+++ 25/A - IQ test/solution.java
@@ -0,0 +1,35 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int n = sc.nextInt();
+        int[] a = new int[n];
+ 
+        int even = 0;
+        int odd = 0;
+ 
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+ 
+            if (a[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+ 
+        for (int i = 0; i < n; i++) {
+            if (even > odd && a[i] % 2 != 0) {
+                System.out.println(i + 1);
+                return;
+            }
+ 
+            if (odd > even && a[i] % 2 == 0) {
+                System.out.println(i + 1);
+                return;
+            }
+        }
+    }
+}

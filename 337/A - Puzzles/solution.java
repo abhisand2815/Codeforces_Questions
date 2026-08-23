380c8e52dad01b96a771788132e19ba29c865ebb
Abhmanyu Singh <abhimanyu15282005@gmail.com>
2026-08-23 08:57:00 +0530

    Add Puzzles [A] from Codeforces

---
+++ 337/A - Puzzles/solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+ 
+        int[] f = new int[m];
+ 
+        for (int i = 0; i < m; i++) {
+            f[i] = sc.nextInt();
+        }
+ 
+        Arrays.sort(f);
+ 
+        int ans = Integer.MAX_VALUE;
+ 
+        for (int i = 0; i <= m - n; i++) {
+            ans = Math.min(ans, f[i + n - 1] - f[i]);
+        }
+ 
+        System.out.println(ans);
+    }
+}

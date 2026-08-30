881413ce86096f023c4e2f193c68f1e9517d4765
Abhmanyu Singh <abhimanyu15282005@gmail.com>
2026-08-30 08:46:30 +0530

    Add Yet Another Two Integers Problem [A] from Codeforces

---
+++ 1409/A - Yet Another Two Integers Problem/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+ 
+            long diff = Math.abs(a - b);
+ 
+            System.out.println((diff + 9) / 10);
+        }
+ 
+        sc.close();
+    }
+}

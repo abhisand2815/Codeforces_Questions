0de6994c8cd9265d45f138de142194e34c2dd5e5
Abhmanyu Singh <abhimanyu15282005@gmail.com>
2026-09-01 08:27:04 +0530

    Add Game With Sticks [A] from Codeforces

---
+++ 451/A - Game With Sticks/solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+ 
+        if (Math.min(n, m) % 2 == 1) {
+            System.out.println("Akshat");
+        } else {
+            System.out.println("Malvika");
+        }
+ 
+        sc.close();
+    }
+}

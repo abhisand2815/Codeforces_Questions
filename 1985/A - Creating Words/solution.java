67c53b548212f543a1bb2faa5051b10183ca6b7f
Abhmanyu Singh <abhimanyu15282005@gmail.com>
2026-09-05 08:39:21 +0530

    Add Creating Words [A] from Codeforces

---
+++ 1985/A - Creating Words/solution.java
@@ -0,0 +1,23 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            String a = sc.next();
+            String b = sc.next();
+ 
+            char[] x = a.toCharArray();
+            char[] y = b.toCharArray();
+ 
+            char temp = x[0];
+            x[0] = y[0];
+            y[0] = temp;
+ 
+            System.out.println(new String(x) + " " + new String(y));
+        }
+    }
+}

50413193a89b94f722e7f60b20163fb0a413826d
Abhmanyu Singh <abhimanyu15282005@gmail.com>
2026-09-08 08:54:51 +0530

    Add Buttons [A] from Codeforces

---
+++ 1858/A - Buttons/solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+ 
+        int t = sc.nextInt();
+ 
+        while (t-- > 0) {
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            long c = sc.nextLong();
+ 
+            if (a > b)
+                System.out.println("First");
+            else if (b > a)
+                System.out.println("Second");
+            else
+                System.out.println(c % 2 == 1 ? "First" : "Second");
+        }
+    }
+}
